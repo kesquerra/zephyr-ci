271452193a01974950d25b13265d1d3c4b8da8f1
kesquerra <esquerrk@oregonstate.edu>
2021-01-16 13:43:48 -0800

    add random keygen, simple encryption

--- zephyr-ci.js
+++ zephyr-ci.js
@@ -1,3 +1,29 @@
-exports.test = function() {
-    console.log("Test for skeleton npm package");
+var crypto = require("crypto-js");
+
+exports.verify = function(key, clientID) {
+    var hmac = crypto.HmacSHA256(key + clientID)
+
+    //TODO: send hmac to API
+
+    //TODO: confirm client
+    
+}
+
+exports.encryptText = function(text, key) {
+    var cipher = crypto.SHA256(text, key).toString();
+    return cipher;
+}
+
+exports.encryptObject = function(object, key) {
+    var cipher = crypto.SHA256(JSON.stringify(object), key).toString();
+    return cipher;
+}
+
+exports.createContentKey = function(clientID) {
+    var token = require('random-token').create(clientID);
+    var key = token(16);
+
+    //TODO: create object to send to API w/ resID, contentID, key
+
+    return key;
 }
