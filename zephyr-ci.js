17f4e4e56d945d65b5269c799a0aba5804438f2e
kesquerra <esquerrk@oregonstate.edu>
2021-01-19 12:48:36 -0800

    add send key to api function, get resource ID from api

--- zephyr-ci.js
+++ zephyr-ci.js
@@ -22,8 +22,36 @@ exports.encryptObject = function(object, key) {
 exports.createContentKey = function(clientID) {
     var token = require('random-token').create(clientID);
     var key = token(16);
-
-    //TODO: create object to send to API w/ resID, contentID, key
-
     return key;
+}
+
+exports.getNewResID = function() {
+    var api = "placeholder.com"
+    fetch(api, {method: "GET"})
+    .then(function (res) {
+        return res.json();
+    })
+}
+
+//TODO: set price for content
+
+exports.sendKey = function(clientID) {
+    //TODO: add confirmation of client
+
+    var key = createContentKey(clientID);
+    var resID = getNewResID(clientID);
+    var payload = {
+        "clientID": clientID,
+        "resID": resID,
+        "key": key
+        //TODO: add price to object
+    };
+
+    fetch(api, {
+        method:"POST",
+        body: JSON.stringify(payload),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
 }
